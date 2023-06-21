6f4f331d828934b4a7276bb9f4e46b3aa0341ac4
andrewgilbert2018 <andrewgilbert2018@gmail.com>
2023-06-21 14:48:33 -0700

    review updated

--- models/review.py
+++ models/review.py
@@ -1,10 +1,14 @@
 #!/usr/bin/python3
-""" Review module for the HBNB project """
-from models.base_model import BaseModel
+""" review class"""
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String, Integer, ForeignKey
 
 
-class Review(BaseModel):
-    """ Review classto store review information """
-    place_id = ""
-    user_id = ""
-    text = ""
+class Review(BaseModel, Base):
+    """ class for Review
+    """
+    __tablename__ = 'reviews'
+
+    place_id = Column(String(60), ForeignKey('places.id'), nullable=False)
+    user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
+    text = Column("text", String(1024), nullable=False)
