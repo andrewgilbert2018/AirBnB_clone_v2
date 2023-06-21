b4118ad41ae7f63a57e1183380dd01ed568ebc68
andrewgilbert2018 <andrewgilbert2018@gmail.com>
2023-06-21 14:13:59 -0700

    amenity updated

--- models/amenity.py
+++ models/amenity.py
@@ -1,7 +1,16 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String, Integer, ForeignKey, Table
+from sqlalchemy.orm import relationship
 
 
-class Amenity(BaseModel):
-    name = ""
+class Amenity(BaseModel, Base):
+    """ Amenity Class
+    Attribs:
+        name: input name
+    """
+    __tablename__ = 'amenities'
+
+    name = Column(String(128), nullable=False)
