3bf103a2074dbd042f960e1b07b20f1c81b9c29f
andrewgilbert2018 <andrewgilbert2018@gmail.com>
2023-07-07 01:36:31 -0700

    1 pack web static

---
+++ 1-pack_web_static.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+a python script Fab module to create a targ file of all my static files
+"""
+from fabric.api import local
+from datetime import datetime
+
+
+def do_pack():
+    """
+    function to pack in .tgz
+    """
+    local("mkdir -p versions")
+    now = datetime.today()
+    try:
+        file_name = "web_static_{}{}{}{}{}{}.tgz".format(now.year, now.month,
+                                                         now.day, now.hour,
+                                                         now.minute,
+                                                         now.second)
+        local("tar -cvzf versions/{} web_static".format(file_name))
+        return (file_name)
+    except:
+        return (None)
